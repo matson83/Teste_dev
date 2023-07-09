7a9d867b3d0dffb67f263a62b9bf635bc1f5b245
Matson junior <86759818+matson83@users.noreply.github.com>
2023-07-09 12:24:45 -0300

    up 0.4

--- app/Http/Controllers/EletroController.php
+++ app/Http/Controllers/EletroController.php
@@ -66,7 +66,7 @@ class EletroController extends Controller
 
         Eletro::findOrFail($id)->delete();
 
-        return redirect('/api');
+        return redirect('/');
 
     }
 
--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::get('/api', [EletroController::class,'index'])->name('index');
+Route::get('/', [EletroController::class,'index'])->name('index');
 
 Route::prefix('app')->group(function(){
 
